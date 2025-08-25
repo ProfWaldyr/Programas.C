7a68a2409249ba015d48bad11a046ce5e3a55cbb
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-08-25 08:39:10 -0300

    Update n1_nc4.c

--- n1_nc4.c
+++ n1_nc4.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int.main(){
-  int.idade;
-  int.quantidade;
-  float.altura;
-  char.letra;
-  char nome[];
+  int. idade = 25;
+  int. quantidade = 1;
+  float. altura = 1.85;
+  double. peso = 88;
+  char. letra ='A';
+  char. nome[20];
 
 
 
