43f98a36df7e80c50e49b18912ce223a75552140
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-01 14:28:29 -0300

    Add files via upload

---
+++ n1_nc4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int.main(){
+int. idade;
+  int. quantidade;
+  float. altura;
+  double. peso;
+  char. letra ;
+  char. nome[20];
+
+  idade = 25;
+  quantidade = 1;
+  alrura = 1.85;
+  peso = 88;
+  letar = 'A';
+  nome [20] = "waldyr";
+
+  //printf("A idade do %s, %d\n", nome, idade);
+  printf("A idade é: %d\n, idade);
+  printf("O nome é: %s\n, nome);
+  printf("A altura é: %.2f\n", altura);
+  printf("A opção é: %c\n, opção);
+
+    /*
+    printf("%formato1 %formato2 %formato3",variavel1, variavel2, variavel3);
+    %d: Imprime inteiro em formato decimal;
+    %i: Equivalente a %d;
+    %f: Imprime um numero de ponto flutuante no formato padrão;
+    %e: Imprime um numero de ponto flutuante na notação cientifica;
+    %c: Imprime um unico caracter;
+    %s: Imprime uma cadeia (string) de cazracter;
+    */  
+
+
+
+
+
+
+
+  
+
+ }
