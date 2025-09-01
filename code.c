dcce9b8607caf85e221a7d7c13d6f299443aa63e
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-01 20:55:04 -0300

    .

--- code.c
+++ code.c
@@ -5,7 +5,20 @@ it main(){
     float altura;
     char nome[50];
 
-    
+    printf("Digite sua idade \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome \n");
+    scanf("%s", &nome);
+
+    printf("digite sua matrícula \n");
+    scanf("%d", &matricula);
+
+
+
 
 
 
