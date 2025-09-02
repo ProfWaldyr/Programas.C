31d3ebf498d3a174ec02002e61b503bfef98f2c4
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-01 21:32:26 -0300

    .

--- desafio_novato.c
+++ desafio_novato.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    int pontos_turisticos; 
+    float PIB;
+    float area_em_km ;
+    char codigo_carta [6];
+    char nome_cidade [20];
+    char estado [20];
+
 
