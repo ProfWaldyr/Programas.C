b42fa2599d700ecad7f8ac9a3eeee372f8bab166
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-09 16:15:55 -0300

    .

--- med_not.c
+++ med_not.c
@@ -1,4 +1,30 @@
 #include <stdio.h>;
 
+int main() {
 
+    int nota_1, nota_2, nota_3;
+    int media;
+    int nota;
 
+    printf("***   PROGRAMA DE CALCULO DE NOTAS    ***\n" );
+
+    printf("Digite quantidade de notas:  \n");
+    scanf("%d", &nota);
+
+    printf("Digite a sua primeira nota:  \n");
+    scanf("%d", &nota_1);
+
+    printf("Digite a sua segunda nota:  \n");
+    scanf("%d", &nota_2);
+
+    printf("Digite a sua terceira nota:  \n");
+    scanf("%d", &nota_3);   
+    
+
+    media = (nota_1 + nota_2 + nota_3) / (nota);
+
+    printf("A média das notas é: %d", media);
+
+   return 0;
+
+}
