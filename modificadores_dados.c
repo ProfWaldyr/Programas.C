30db0c39d1c66e5ade8bbbe908d27f95c6753705
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-10 09:41:24 -0300

    Update modificadores_dados.c

--- modificadores_dados.c
+++ modificadores_dados.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main (){
+  insigned long int numeroGrandePositivo = 4000000000;
+  insigned int numeroPositivo = 4000000000;
+  long long int numeroGrande = 4000000000;
+  int numero = 4000000000;
+
+  printf("Numero Grande Positivo: %lu \n",numeroGrandePositivo);
+  printf("Número Positivo: %lu \n", numeroPositivo);
+  printf("Número Grande: %lu \n", mnumeroGrande);
+  printf("Número: %lu \n", numero);
+
+  return: 0;
+
+}
