bc4488274dd03e581bd866e76e274292819a1b6c
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-10 15:34:05 -0300

    .

---
+++ num_prec.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    double numeroPreciso = 3,14964738273645;
+    long double numeroMuitoPreciso = 3,14964738273645123456;
+
+    printf("Núnero preciso: (double) %.15f\n", numeroPreciso);
+    printf("Número muito preciso: (long double) ")
+
+
+
+
+
+}
