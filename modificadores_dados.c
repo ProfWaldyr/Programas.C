e2c1e7b5793915668cb3676a10685ca9a2d16995
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-10 09:45:38 -0300

    .

--- modificadores_dados.c
+++ modificadores_dados.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main (){
-  insigned long int numeroGrandePositivo = 4000000000;
-  insigned int numeroPositivo = 4000000000;
+int main() {
+  unsigned long int numeroGrandePositivo = 4000000000;
+  unsigned int numeroPositivo = 4000000000;
   long long int numeroGrande = 4000000000;
   int numero = 4000000000;
 
   printf("Numero Grande Positivo: %lu \n",numeroGrandePositivo);
-  printf("Número Positivo: %lu \n", numeroPositivo);
-  printf("Número Grande: %lu \n", mnumeroGrande);
-  printf("Número: %lu \n", numero);
+  printf("Número Positivo: %u \n", numeroPositivo);
+  printf("Número Grande: %lld \n", mnumeroGrande);
+  printf("Número: %d \n", numero);
 
   return: 0;
 
