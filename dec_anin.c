2ed2c8d676f7dfad43ed52118c8185146793620d
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-16 09:37:43 -0300

    .

--- dec_anin.c
+++ dec_anin.c
@@ -1,1 +1,15 @@
+   #include <stdio.h>
+
+int main(){
+
+    int int idade;
+    float renda;
+
+// programa para verificar se uma pessoa está qualificada 
+// para um desconto especial com base na idade e na renda mensal
+// A pessoa deve ter mais de 60 anos e menos de 18 e uma renda
+// menor que 2000 mil;
+
+
+
 
