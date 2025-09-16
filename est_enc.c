ddfefaff62b9f5ccbfcad7a429f980e7dfcf5418
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-16 08:59:01 -0300

    .

--- est_enc.c
+++ est_enc.c
@@ -1,1 +1,32 @@
+   #include <stdio.h>
 
+int main(){
+
+    int idade;
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+
+    // crianca < 12;
+    // adolescente >= 12 && < 18;
+    // adulto >= 18 && < 60;
+    // idoso >=60;
+    
+    if(idade <12)
+    {printf("Você é um criança mimada \n");
+    }
+    
+    else if(idade>=12 && idade <18)
+    {printf("Você é um adolescente \n");
+    }
+
+    else if(idade>=18 && idade <60)
+    {printf("Você é adulto irresponsável \n");
+    }
+
+    else {printf("Você é idoso, tá ferrado no Brasil \n");
+        
+    }
+
+}
