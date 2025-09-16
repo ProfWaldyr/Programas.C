3950adf4ba9656b9a85751643d52c262d7688d0a
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-16 08:32:30 -0300

    .

--- op_log_comb.c
+++ op_log_comb.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 25;
+    float altura = 1.82;
+
+    if( idade >= 25 && idade <= 30 && altura >= 1.82);
+    
+    {printf("Peso e altura estçao de acordo com o especificado \n");
+    }
+    
+    else
+    {printf("Peso e altura fora do especificado\n");
+    }
+
+}
+
+
+
+
+
 
