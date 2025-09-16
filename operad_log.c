9a0a605fb78c9c29bb5dad0fbae8ce0a7a43710e
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-16 08:00:28 -0300

    .

--- operad_log.c
+++ operad_log.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = -10 , b = 4;
+
+    if ( a > 0 && b > 0)
+    
+    { printf("Os dois numeros são positivos \n");
+    }
+
+    else
+    {printf("Pelo menos um dos numeros é negativo \n");
+    }
+
+     }
+
+
+
 
