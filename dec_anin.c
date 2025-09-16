308ce19ad4c9c402f8aa83323912856de1fc7062
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-16 09:41:20 -0300

    .

--- dec_anin.c
+++ dec_anin.c
@@ -10,6 +10,17 @@ int main(){
 // A pessoa deve ter mais de 60 anos e menos de 18 e uma renda
 // menor que 2000 mil;
 
+printf( "Digite a sua idade: \n");
+scanf ("%d",&idade);
+
+printf( "Digite a sua renda mensal: \n");
+scanf ("%d",&idade);
+
+
+
+}
+
+
 
 
 
