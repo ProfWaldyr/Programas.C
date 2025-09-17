de6bf7716a28baaef89f9785802de43a218400a0
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-17 14:31:05 -0300

    .

--- dec_anin.c
+++ dec_anin.c
@@ -1,28 +1,36 @@
-   #include <stdio.h>
+      #include <stdio.h>
 
 int main(){
 
-    int int idade;
+    int idade;
     float renda;
 
 // programa para verificar se uma pessoa está qualificada 
 // para um desconto especial com base na idade e na renda mensal
-// A pessoa deve ter mais de 60 anos e menos de 18 e uma renda
+// A pessoa deve ter mais de 60 anos ou menos de 18 e uma renda
 // menor que 2000 mil;
 
 printf( "Digite a sua idade: \n");
 scanf ("%d",&idade);
 
 printf( "Digite a sua renda mensal: \n");
-scanf ("%d",&renda);
-
-if( idade >)
+scanf ("%f",&renda);
 
 
+if(idade >60 && renda <=2000) 
+
+{printf("Você possui benefício para receber \n");
+
+}
+
+if (idade <=18 && renda <=2000)
+
+{printf("Você possui benefício para receber \n");
 
 }
 
 
+else{printf("Você não possui direito ao benefício");}
 
 
-
+}
