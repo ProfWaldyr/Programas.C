a102a8d2c4189d884e71be018a7aac8b685d20d3
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-10-15 14:28:06 -0300

    .

--- do_while.c
+++ do_while.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+
+    do {
+
+    printf("Digite um numero par para sair do programa....");
+    scanf("%d", &numero);
+
+    if(numero % 2 == 0){
+        printf("%d é par!" , numero);
+    }    
+        else{
+        printf("%d é ímpar!" , numero);
+    }
+
+} while(numero % 2 != 0);
+
+printf("Você digitou um número par! Saindo do programa...");    
+
+return 0;
+}
